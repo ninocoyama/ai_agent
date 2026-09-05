a270ca1d24bfa81e1fb3cd06e56025a28fa53537
ninocoyama <nino.coyamaa@gmail.com>
2026-09-05 22:25:41 +0900

    B: Setting main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if api_key == None:
+    raise RuntimeError("Check the API Key")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+)
+
+print(response.choices[0].message.content)
+
 def main():
     print("Hello from aiagent!")
 
