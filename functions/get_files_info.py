3f183b3d64ffcf6348d9256e94593f837255230e
ninocoyama <nino.coyamaa@gmail.com>
2026-09-06 12:21:31 +0900

    added get_files_info func

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,6 +10,13 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if os.path.isdir(target_dir) is False:
             return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
+
+        file_info_list = []
+        for file in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, file)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            file_info_list.append(f"- {file}: file_size={file_size} bytes, is_dir={is_dir}")
+        return "\n".join(file_info_list)
     except Exception as error:
         return f"Error: {error}"
