f9f5001401319326d0f29e9921562648fc6ac076
ninocoyama <nino.coyamaa@gmail.com>
2026-09-06 09:33:56 +0900

    verbose added

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import argparse
 
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
-if api_key == None:
-    raise RuntimeError("Check the API Key")
-
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
-
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=[
-        {
-            "role": "user",
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-        }
-    ],
-)
-
-print(response.choices[0].message.content)
 
 def main():
-    print("Hello from aiagent!")
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Check the API Key")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+
+    if response.usage is None:
+        raise RuntimeError("API request failed")
+    if args.verbose is True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    print(f"Response:\n{response.choices[0].message.content}")
 
 if __name__ == "__main__":
     main()
